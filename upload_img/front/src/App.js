32199f564ac84e78dd5c8f23506ff737c5b0974d
minseok <kms9887@naver.com>
2021-10-21 13:54:51 +0900

    이미지 업로드 위한 frontend

---
+++ upload_img/front/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function App() {
+  const [files, setFiles] = useState({});
+
+  const makeFileList = (e) => {
+    setFiles(e.target.files);
+  }
+
+  const uploadImages = () => {
+    const formData = new FormData();
+    Object.values(files).forEach((file)=>{
+      formData.append('uploadFiles', file);
+    });
+
+    fetch('/api/img',{
+      method: 'POST',
+      body: 'formData'
+    }).then(res => {
+      res.json();
+    }).then(data => {
+      console.log(data);
+    })
+  }
+
+  return (
+    <div>
+      <input type='file' accept='image/*'  multiple onChange={makeFileList}/>
+      <button onClick={uploadImages}>업로드</button>
+    </div>
+  );
+}
+
+export default App;
