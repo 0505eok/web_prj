00de6f5868e642d08e19d380b4b0697b4591d711
minseok <kms9887@naver.com>
2021-10-29 00:12:31 +0900

    FormData 를 활용한 이미지 전송

--- upload_img/front/src/App.js
+++ upload_img/front/src/App.js
@@ -1,32 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
   const [files, setFiles] = useState(null);
 
   const makeFileList = (e) => {
     setFiles(e.target.files);
-  }
+  };
 
   const uploadImages = () => {
     const formData = new FormData();
-    Object.values(files).forEach((file)=>{
-      formData.append('uploadFiles', file);
+    Object.values(files).forEach((file) => {
+      formData.append("uploadFiles", file);
     });
 
-    console.log(formData);
-    fetch('/img',{
-      method: 'POST',
-      body: formData
-    }).then(res => {
-      res.json();
-    }).then(data => {
-      console.log(data);
+    fetch("/img", {
+      method: "POST",
+      body: formData,
     })
-  }
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        //console.log(data);
+      });
+  };
 
   return (
     <div>
-      <input type='file' accept='image/*'  multiple onChange={makeFileList}/>
+      <input type="file" accept="image/*" multiple onChange={makeFileList} />
       <button onClick={uploadImages}>업로드</button>
     </div>
   );
