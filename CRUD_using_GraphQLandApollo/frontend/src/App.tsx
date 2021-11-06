0d06a78f62b8608463702d3664ff8f135db66caa
minseok <kms9887@naver.com>
2021-11-06 21:51:07 +0900

    아폴로 클라이언트 사용을 위한 기본 설정

--- CRUD_using_GraphQLandApollo/frontend/src/App.tsx
+++ CRUD_using_GraphQLandApollo/frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_BASE_URI,
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <ApolloProvider client={client}>hello</ApolloProvider>
+    </div>
+  );
 }
 
 export default App;
